e38a1c727e19aff494cb442ffe9a5c41fbaa85f8
CRenwick57 <crenwick57@gmail.com>
2025-09-06 11:40:34 +0100

    Initial Development

---
+++ event-planner/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../routes/Navbar"
+
+export default function Home(){
+    return(
+        <>
+        <div>
+            <h1>Hello world!</h1>
+        </div>
+        </>
+    )
+}
---
+++ event-planner/src/routes/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/" className="nav_link">
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link to="/AddEvent" className="nav_link">
+              Add Event
+            </Link>
+          </li>
+          <li>
+            <Link to="/Help" className="nav_link">
+              Help
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
